205af4b714fcd94688b1af66e3fb58f2b11f6790
KyleWilliamz <kjw22@buffalo.edu>
2017-12-25 19:22:28 -0500

    Update
    Source code to run the Arduino withtthe Camera, Heart Rate, and SD Card Reader modules and process the correct information.

---
+++ HRBodyCamera.ino
@@ -0,0 +1,144 @@
+#include <Adafruit_VC0706.h>
+#include <SPI.h>
+#include <SD.h>
+
+#include <SoftwareSerial.h>         
+
+// Adafruit SD shields and modules: pin 10
+
+#define chipSelect 10
+
+// Using SoftwareSerial (Arduino 1.0+) or NewSoftSerial (Arduino 0023 & prior):
+#if ARDUINO >= 100
+// On Uno: camera TX connected to pin 2, camera RX to pin 3:
+SoftwareSerial cameraconnection = SoftwareSerial(2, 3);
+
+#else
+NewSoftSerial cameraconnection = NewSoftSerial(2, 3);
+#endif
+Adafruit_VC0706 cam = Adafruit_VC0706(&cameraconnection);
+
+// Using hardware serial on Mega: camera TX conn. to RX1,
+// camera RX to TX1, no SoftwareSerial object is required:
+//Adafruit_VC0706 cam = Adafruit_VC0706(&Serial1);
+
+
+void setup() {
+
+  // When using hardware SPI, the SS pin MUST be set to an
+  // output (even if not connected or used).  If left as a
+  // floating input w/SPI on, this can cause lockuppage.
+#if !defined(SOFTWARE_SPI)
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  if(chipSelect != 53) pinMode(53, OUTPUT); // SS on Mega
+#else
+  if(chipSelect != 10) pinMode(10, OUTPUT); // SS on Uno, etc.
+#endif
+#endif
+
+  Serial.begin(9600);
+  pinMode(5, INPUT); // Setup for leads off detection LO +
+  pinMode(6, INPUT); // Setup for leads off detection LO -
+  Serial.println("VC0706 Camera test");
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }  
+  
+  // Try to locate the camera
+  if (cam.begin()) {
+    Serial.println("Camera Found:");
+  } else {
+    Serial.println("No camera found?");
+    return;
+  }
+  // Print out the camera version information (optional)
+  char *reply = cam.getVersion();
+  if (reply == 0) {
+    Serial.print("Failed to get version");
+  } else {
+    Serial.println("-----------------");
+    Serial.print(reply);
+    Serial.println("-----------------");
+  }
+
+  // Set the picture size - 640x480, 320x240 or 160x120 
+  
+  //cam.setImageSize(VC0706_640x480);        // biggest
+  cam.setImageSize(VC0706_320x240);        // medium
+  //cam.setImageSize(VC0706_160x120);          // small
+
+//  uint8_t imgsize = cam.getImageSize();
+//  Serial.print("Image size: ");
+//  if (imgsize == VC0706_640x480) Serial.println("640x480");
+//  if (imgsize == VC0706_320x240) Serial.println("320x240");
+//  if (imgsize == VC0706_160x120) Serial.println("160x120");
+
+
+  //  Motion detection system can alert you when the camera 'sees' motion!
+  cam.setMotionDetect(true);           // turn it on
+  //cam.setMotionDetect(false);        // turn it off   (default)
+
+  // verify whether motion detection is active!
+  Serial.print("Motion detection is ");
+  if (cam.getMotionDetect()) 
+    Serial.println("ON");
+  else 
+    Serial.println("OFF");
+}
+
+
+
+
+void loop() {
+ if (cam.motionDetected() && (analogRead(A0) > 80)) {
+   Serial.println("Motion!");
+   Serial.print("Heart Rate: ");
+   Serial.println(analogRead(A0));   
+   cam.setMotionDetect(false);
+   
+  if (! cam.takePicture()) 
+    Serial.println("Failed to snap!");
+  else 
+    Serial.println("Picture taken!");
+  
+  char filename[13];
+  strcpy(filename, "IMAGE00.JPG");
+  for (int i = 0; i < 100; i++) {
+    filename[5] = '0' + i/10;
+    filename[6] = '0' + i%10;
+    // create if does not exist, do not open existing, write, sync after write
+    if (! SD.exists(filename)) {
+      break;
+    }
+  }
+  
+  File imgFile = SD.open(filename, FILE_WRITE);
+  
+  uint16_t jpglen = cam.frameLength();
+  Serial.print(jpglen, DEC);
+  Serial.println(" byte image");
+ 
+  Serial.print("Writing image to "); Serial.print(filename);
+  
+  while (jpglen > 0) {
+    // read 32 bytes at a time;
+    uint8_t *buffer;
+    uint8_t bytesToRead = min(32, jpglen);
+    buffer = cam.readPicture(bytesToRead);
+    imgFile.write(buffer, bytesToRead);
+
+    //Serial.print("Read ");  Serial.print(bytesToRead, DEC); Serial.println(" bytes");
+
+    jpglen -= bytesToRead;
+  }
+  imgFile.close();
+  Serial.println("...Done!");
+  cam.resumeVideo();
+  cam.setMotionDetect(true);
+ }
+}
+
